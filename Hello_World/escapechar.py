aeeb88d947c00096c17b188ec29b6fd15392dd15
Ashish Deshpande <ashishdeshpande91@gmail.com>
2021-03-22 21:56:58 +0530

    Escape string character

--- Hello_World/escapechar.py
+++ Hello_World/escapechar.py
@@ -5,3 +5,5 @@ tabbedString = "1\t2\t3\t4\t5"
 print(tabbedString)
 
 print('The pet shop owner said "No, no, \'e\'s uh,...he\'s resting".')
+#or
+print("THe pet shop owner said \"No, no, 'e's uh,...he's resting\".")
