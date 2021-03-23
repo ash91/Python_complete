4de3af3cac2af088314dae7f6282a1e5389f1536
Ashish Deshpande <ashishdeshpande91@gmail.com>
2021-03-23 09:55:58 +0530

    Variable declaration

---
+++ Hello_World/Variables.py
@@ -0,0 +1,19 @@
+"""Variables are nothing but reserved memory locations to store values. This means that when you create a variable you reserve some space in memory.
+
+Based on the data type of a variable, the interpreter allocates memory and decides what can be stored in the reserved memory. Therefore, by assigning different data types to variables, you can store integers, decimals or characters in these variables.
+Assigning Values to Variables
+
+Python variables do not need explicit declaration to reserve memory space. The declaration happens automatically when you assign a value to a variable. The equal sign (=) is used to assign values to variables.
+
+The operand to the left of the = operator is the name of the variable and the operand to the right of the = operator is the value stored in the variable. For example −"""
+
+
+#!/usr/bin/python
+
+counter = 100          # An integer assignment
+miles   = 1000.0       # A floating point
+name    = "John"       # A string
+
+print(counter)
+print(miles)
+print(name)
