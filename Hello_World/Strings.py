9a5cd05a2e00557f3977a0eabe9168f8ba27655b
Ashish Deshpande <ashishdeshpande91@gmail.com>
2021-03-25 11:35:31 +0530

    Changes made to string

--- Hello_World/Strings.py
+++ Hello_World/Strings.py
@@ -1,4 +1,4 @@
-print("Today is a good day to learn Python")
+print("learn Python")
 print('Python is fun')
 print("Python's string are easy to use")
 print('We can even include "quotes" in strings')
