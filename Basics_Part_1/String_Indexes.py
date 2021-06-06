e86c942006776429ade8d8f9fe7daaae18bc634c
Ashish Deshpande <ashishdeshpande91@gmail.com>
2021-06-06 21:05:37 +0530

    Added string methods

--- Basics_Part_1/String_Indexes.py
+++ Basics_Part_1/String_Indexes.py
@@ -1,4 +1,8 @@
 fruit = 'Pineapple'
-
-print(fruit[:-4:2])
+fruit += 's'
+print(fruit.upper())
+# Syntax for string sliciing
+# variablename[start:stop:stepover]
+print(fruit[:-6:-2])
 print(fruit[-5:])
+print(fruit[::-2])
