5a2e2748a0a76857871991f8c0a23c27ea822312
Ashish Deshpande <ashishdeshpande91@gmail.com>
2021-06-06 17:51:16 +0530

    Added string concatenation

--- Basics_Part_1/Strings.py
+++ Basics_Part_1/Strings.py
@@ -1,12 +1,19 @@
 print("learn Python")
-print('Python is fun')
+print(type('Python is fun'))
 print("Python's string are easy to use")
-print('We can even include "quotes" in strings')
+long_string='''
+WOW
+0 0
+---
+'''
+print(long_string)
+
+# String Concatenation
 print("hello" + " world")
 greeting = "Hello"
-
+number=str(5)
 # Input funtion =Read a string from standard input. The trailing newline is stripped.
 name = input("Please enter your name ")
-print(greeting + name)
+
 # if we want a space, we can add that too
-print(greeting + ' ' + name)
+print(greeting + ' ' + name + ' ' + number)
