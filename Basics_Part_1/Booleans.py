e659d325dc6da9eedd049297502c6ef5b6f38c6d
Ashish Deshpande <ashishdeshpande91@gmail.com>
2021-06-06 21:48:14 +0530

    Booleans

---
+++ Basics_Part_1/Booleans.py
@@ -0,0 +1,7 @@
+#booleans
+is_num = True
+is_all = False
+print(is_num)
+print(is_all)
+print(bool(1))
+print(bool('True'))
--- Basics_Part_1/String_Indexes.py
+++ Basics_Part_1/String_Indexes.py
@@ -1,6 +1,9 @@
 fruit = 'Pineapple'
-fruit += 's'
+
+# String Methods used with .
 print(fruit.upper())
+print(fruit[1:len(fruit)])
+print(fruit.replace('Pine','Custard'))
 # Syntax for string sliciing
 # variablename[start:stop:stepover]
 print(fruit[:-6:-2])
