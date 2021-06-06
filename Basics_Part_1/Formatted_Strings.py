cee99e7ea518dcb1ab1e5225b4a9081098780d0c
Ashish Deshpande <ashishdeshpande91@gmail.com>
2021-06-06 18:08:24 +0530

    Added formatted string

---
+++ Basics_Part_1/Formatted_Strings.py
@@ -0,0 +1,10 @@
+name = 'Ashish'
+age = 30
+print(name)
+
+# string concatenation style
+print(f'Hi' + ' ' + name + '.You are ' + str(age) + ' Years old')
+
+# formatted string style
+
+print(f'Hi {name}. You are {age} years old')
