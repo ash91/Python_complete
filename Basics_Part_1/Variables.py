f1df8f2bafa95224b71bb5d6c3a47fe5b0591dac
Ashish Deshpande <ashishdeshpande91@gmail.com>
2021-06-06 17:37:27 +0530

    Augmented Assignment Operator

--- Basics_Part_1/Variables.py
+++ Basics_Part_1/Variables.py
@@ -7,11 +7,25 @@ Python variables do not need explicit declaration to reserve memory space. The d
 
 The operand to the left of the = operator is the name of the variable and the operand to the right of the = operator is the value stored in the variable. For example −"""
 
+# snake_case
+# Start with lowercase or underscore
+# Letters, numbers & underscores
+# Case Sensitive
+# Don't overwrite keywords
 
+user_id = 121          # Snake case
 counter = 100          # An integer assignment
 miles   = 1000.0       # A floating point
 name    = "John"       # A string
+a,b,c = 1,2,3
 
+print(user_id)
 print(counter)
 print(miles)
 print(name)
+
+# Augmented Assignment Operator
+
+x = 5
+x += 20
+print(x)
