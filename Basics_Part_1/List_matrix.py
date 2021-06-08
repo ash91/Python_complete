818acdb2d13cc73b1b04de0c68ef642544658a46
Ashish Deshpande <ashishdeshpande91@gmail.com>
2021-06-08 07:36:15 +0530

    Added List Matrix

---
+++ Basics_Part_1/List_matrix.py
@@ -0,0 +1,13 @@
+'''
+What is Matrix in Python?
+
+These are 2D (two dimensional) data structure. In live projects and real data simulation, you have to keep the data in a sequential or tabular format. Let suppose you want to store data of three employees of three different departments. So in tabular format, it will look something like:
+
+Employee ID	Employee Name	Employee Dept
+1001A	    Ray	Technical   Head
+2004B	    Karlos	        Manager
+3100A	    Alex	        Lead Developer
+
+In Python, these tables are termed as two-dimensional arrays, which are also called matrices. Python gives us the facility to represent these data in the form of lists.'''
+
+val = [['1001A','Ray', 'Technical Head'], ['2004B', 'Karlos' , 'Manager'], ['3100A', 'Alex' , 'Lead Developer']]
