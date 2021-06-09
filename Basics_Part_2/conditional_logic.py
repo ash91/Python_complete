57d804b76791dba0b16ca5940273219e626ac421
Ashish Deshpande <ashishdeshpande91@gmail.com>
2021-06-09 14:51:57 +0530

    Added conditional Logic

---
+++ Basics_Part_2/conditional_logic.py
@@ -0,0 +1,9 @@
+is_old =True
+is_licenses = False
+
+if is_old and is_licenses:
+    print("You are old enough to drive")
+
+    print("You can drive now")
+else:
+    print("Sorry you are underage")
