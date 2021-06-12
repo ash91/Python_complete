16b052b304af212faf914546aa625427a9fc0493
Ashish Deshpande <ashishdeshpande91@gmail.com>
2021-06-12 22:34:45 +0530

    Enumerate

--- Basics_Part_2/For_loops.py
+++ Basics_Part_2/For_loops.py
@@ -10,4 +10,34 @@
 for i in [1,1,2,3,4,5,6,7,8,9]:
     for x in ['a','b','c']:
         print(i,x)
-  
+
+user = {
+    'name': 'ashish',
+    'age' : 30,
+    'cant_swim' : True
+}
+
+for key,value in user.items():
+    print(key,value)
+for item in user.keys():
+    print(item)
+for item in user.values():
+    print(item)
+
+
+# Counter Exercise
+
+my_list = [1,2,3,4,5,6,7,8,9]
+count = 0
+for items in my_list:
+    count= count+items
+print(count)
+
+for number in range(10,0,-2):
+    print(number)
+
+# Enumerate
+
+for i,char in enumerate((list(range(100,0,-2)))):
+    if char == 50:
+        print(f'Index of 50 is: {i}')
