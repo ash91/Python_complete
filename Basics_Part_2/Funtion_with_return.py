6c26e4ba613ca113d4b8daca8b48a6b4b474f869
Ashish Deshpande <ashishdeshpande91@gmail.com>
2021-06-13 08:49:28 +0530

    Docstring Added

--- Basics_Part_2/Funtion_with_return.py
+++ Basics_Part_2/Funtion_with_return.py
@@ -2,11 +2,15 @@
 
 def sum(num1, num2):
     def another_sum(n1, n2):
+        '''
+        Info: Defining another function # This is a docstring.
+        '''
         return n1+n2
     return another_sum(num1,num2)
 
 total =sum(10,20)
 print(total)
+
 
 
 # Tesla Exercise
